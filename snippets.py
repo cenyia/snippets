0563eeae8d3a07ec407e864f5887bbfe9aeb292d
cenyia <chidienyia@hotmail.com>
2016-01-06 04:14:59 +0000

    get put set

--- snippets.py
+++ snippets.py
@@ -39,7 +39,22 @@ def main():
     put_parser.add_argument("name", help="The name of the snippet")
     put_parser.add_argument("snippet", help="The snippet text")
     
+    # Subparser for the get command
+    logging.debug("Constructing get subparser")
+    get_parser = subparsers.add_parser("get", help="Store a name")
+    get_parser.add_argument("name", help="The name of the snippet")
+    
     arguments = parser.parse_args(sys.argv[1:])
+    # Convert parsed arguments from Namespace to dictionary
+    arguments = vars(arguments)
+    command = arguments.pop("command")
+    
+    if command == "put":
+        name, snippet = put(**arguments)
+        print("Stored {!r} as {1r}". format(snippet, name))
+    elif command == "get":
+        snippet = get(**arguments)
+        print("Retrieved snippet: {!r}".format(snippet))
 
 if __name__ == "__main__":
     main()
